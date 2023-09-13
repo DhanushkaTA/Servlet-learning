9d8a83c80c3cde34c3d685dc10a5924b04441466
DhanushkaTA <dhanushka999ta@gmail.com>
2023-09-13 17:01:15 +0530

    Ensure that we can use objects in the servlet context between servlets

---
+++ _08_JBCP/src/ItemServlet.java
@@ -0,0 +1,31 @@
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@WebServlet(urlPatterns = "/item")
+public class ItemServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletContext servletContext = req.getServletContext();
+        BasicDataSource bds = (BasicDataSource) servletContext.getAttribute("bds");
+
+        try {
+            Connection connection = bds.getConnection();
+            ResultSet resultSet = connection.prepareStatement("SELECT * FROM item").executeQuery();
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString(1));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
