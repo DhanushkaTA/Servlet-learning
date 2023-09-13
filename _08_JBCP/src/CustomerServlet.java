01ed4a7a9fc182634bff1485f83fd6329ca7c10b
DhanushkaTA <dhanushka999ta@gmail.com>
2023-09-13 16:31:07 +0530

    Ensure that we can use the objects in the servlet context within the servlet itself =

--- _08_JBCP/src/CustomerServlet.java
+++ _08_JBCP/src/CustomerServlet.java
@@ -1,5 +1,9 @@
 import org.apache.commons.dbcp2.BasicDataSource;
 
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -29,6 +34,9 @@ public class CustomerServlet extends HttpServlet {
         bts.setMaxTotal(5);//add how many connection we want
         bts.setInitialSize(5);//set how many connection we should initialize
 
+        ServletContext servletContext = req.getServletContext();//get servlet context
+        servletContext.setAttribute("bds",bts);//add bds to servlet context
+
         try {
             Connection connection = bts.getConnection();//get connection
             ResultSet resultSet = connection.prepareStatement("SELECT * FROM customer").executeQuery();
@@ -38,5 +46,40 @@ public class CustomerServlet extends HttpServlet {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletContext servletContext = req.getServletContext();
+        BasicDataSource bds = (BasicDataSource) servletContext.getAttribute("bds");
+
+        resp.setContentType("application/json");
+
+        JsonReader reader = Json.createReader(req.getReader());
+        JsonObject jsonObject = reader.readObject();
+        String id = jsonObject.getString("id");
+        String name = jsonObject.getString("name");
+        String address = jsonObject.getString("address");
+        double salary = jsonObject.getInt("salary");
+
+        String sql="INSERT INTO customer VALUES(?,?,?,?)";
+
+        try {
+            Connection connection = bds.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setObject(1,id);
+            preparedStatement.setObject(2,name);
+            preparedStatement.setObject(3,address);
+            preparedStatement.setObject(4,salary);
+            int i = preparedStatement.executeUpdate();
+            if (i>0) {
+                System.out.println("customer saved");
+            }else {
+                System.out.println("customer not saved");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
