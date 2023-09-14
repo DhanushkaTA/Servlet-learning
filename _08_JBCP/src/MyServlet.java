642a322da5147a525ba74d7d359044ad2919c5fd
DhanushkaTA <dhanushka999ta@gmail.com>
2023-09-14 14:21:38 +0530

    Life cycle of servlet

---
+++ _08_JBCP/src/MyServlet.java
@@ -0,0 +1,21 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = "/servlet")
+public class MyServlet extends HttpServlet {
+    //Life cycle of servlet
+    public MyServlet() {
+        System.out.println("create MyServlet object");
+    }
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("initialize as a Servlet");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("Destroy the servlet");
+    }
+}
